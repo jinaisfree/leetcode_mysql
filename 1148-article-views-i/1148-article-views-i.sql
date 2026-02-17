329f7a126bd58cb9aa3b3e95c96bed8a10a50224
Jina Seo <104765262+jinaisfree@users.noreply.github.com>
2026-02-17 15:55:28 +0900

    Time: 460 ms (18.31%), Space: 0B (100%) - LeetHub

--- 1148-article-views-i/1148-article-views-i.sql
+++ 1148-article-views-i/1148-article-views-i.sql
@@ -2,11 +2,14 @@
 select
     distinct v1.author_id as id
 from Views v1
-where exists (
-    select
-        1
-    from Views v2
-    where v1.author_id = v2.viewer_id
-    and v1.article_id = v2.article_id
-)
+join Views v2
+on v1.article_id = v2.article_id
+and v1.author_id = v2.viewer_id
+-- where exists (
+--     select
+--         1
+--     from Views v2
+--     where v1.author_id = v2.viewer_id
+--     and v1.article_id = v2.article_id
+-- )
 order by id
