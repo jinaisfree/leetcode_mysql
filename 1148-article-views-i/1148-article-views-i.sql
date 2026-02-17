ab54dbe37f009e8d697a1d140528ee8e16cd245d
Jina Seo <104765262+jinaisfree@users.noreply.github.com>
2026-02-17 16:18:36 +0900

    Time: 504 ms (11.43%), Space: 0B (100%) - LeetHub

--- 1148-article-views-i/1148-article-views-i.sql
+++ 1148-article-views-i/1148-article-views-i.sql
@@ -1,21 +1,21 @@
 # Write your MySQL query statement below
-select
-    distinct v1.author_id as id
-from Views v1
-join Views v2
-on v1.author_id = v2.viewer_id
-and v1.article_id = v2.article_id
--- where exists (
---     select
---         1
---     from Views v2
---     where v1.author_id = v2.viewer_id
---     and v1.article_id = v2.article_id
--- )
-order by id
-
 -- select
 --     distinct v1.author_id as id
--- from Views
--- where author_id = viewer_id
+-- from Views v1
+-- join Views v2
+-- on v1.author_id = v2.viewer_id
+-- and v1.article_id = v2.article_id
+-- -- where exists (
+-- --     select
+-- --         1
+-- --     from Views v2
+-- --     where v1.author_id = v2.viewer_id
+-- --     and v1.article_id = v2.article_id
+-- -- )
 -- order by id
+
+select
+    distinct author_id as id
+from Views
+where author_id = viewer_id
+order by id
