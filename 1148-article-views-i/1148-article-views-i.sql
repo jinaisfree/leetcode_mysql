2e2b48a2126cfb107c28bd05267dd6a8027e27af
Jina Seo <104765262+jinaisfree@users.noreply.github.com>
2026-02-17 16:05:20 +0900

    Time: 396 ms (65.93%), Space: 0B (100%) - LeetHub

--- 1148-article-views-i/1148-article-views-i.sql
+++ 1148-article-views-i/1148-article-views-i.sql
@@ -3,8 +3,8 @@ select
     distinct v1.author_id as id
 from Views v1
 join Views v2
-on v1.article_id = v2.article_id
-where v1.author_id = v2.viewer_id
+on v1.author_id = v2.viewer_id
+and v1.article_id = v2.article_id
 -- where exists (
 --     select
 --         1
