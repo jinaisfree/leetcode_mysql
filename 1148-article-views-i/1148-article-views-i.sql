9c7163c4e48d23a6811975c8a629ef065af16c3a
Jina Seo <104765262+jinaisfree@users.noreply.github.com>
2026-02-17 16:04:23 +0900

    Time: 382 ms (81.58%), Space: 0B (100%) - LeetHub

--- 1148-article-views-i/1148-article-views-i.sql
+++ 1148-article-views-i/1148-article-views-i.sql
@@ -4,7 +4,7 @@ select
 from Views v1
 join Views v2
 on v1.article_id = v2.article_id
-and v1.author_id = v2.viewer_id
+where v1.author_id = v2.viewer_id
 -- where exists (
 --     select
 --         1
